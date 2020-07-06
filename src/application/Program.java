bc279d659de02713986991385afa0f97aa30bd72
Ricardo Martins <rs84martins@gmail.com>
2020-07-05 22:38:51 -0300

    Implementado FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,27 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] lines = new String[] {"Good morning", 
+								       "Good afternoon", 
+								       "Good night"};
+		
+		String path = "c:\\temp\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 
 	}
 
